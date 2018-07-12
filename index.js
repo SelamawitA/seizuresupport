f4d2073450eb94a7fab7bdd174c7980bc41d3fef
SelamawitA <Selamawit.ainalem@gmail.com>
2018-07-12 09:49:13 -0700

    removed async from intents.

--- index.js
+++ index.js
@@ -1,38 +1,19 @@
 
+
 "use strict";
 
 let Alexa = require("alexa-sdk");
 let AWS = require('aws-sdk');
 const APP_ID = 'amzn1.ask.skill.6f4eefbb-f0aa-474f-82c6-9394bcb2335c';
 let ddb = new AWS.DynamoDB.DocumentClient();
-let userLocation
+let userLocation;
 let handlers = {
   'LaunchRequest': function(){
-    //this.emit(":ask", "Welcome to seizure support, a voice enabled seizure tracker, say your location to complete setup.");
-    this.response.speak("Welcome to seizure support, a voice enabled seizure tracker, say your location to complete setup.")
+    // this.emit(":ask", "Welcome to seizure support, a voice enabled seizure tracker.");
+    this.response.speak("Welcome to seizure support a voice enabled seizure tracker.")
     this.emit(':responseReady');
   },
-  // 'citiesIntent': function(){
-  //   //////////////////////////////////////////////////////////////////////
-  //   // user responds with their city location after the onLaunch event.  //
-  //   // After this point, the stopRecordingIntent doesn't work - recordIntent and seizureTypeIntent both still function //
-  //   ////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-  //     // var map = [ this.event.request.intent.slots.UScity.value, this.event.request.intent.slots.GBcity.value, this.event.request.intent.slots.Europe.value ];
-
-  //     // for (var i=0; i < map.length; i++){
-  //     //   if (map[i]!= null){
-  //     //     userLocation = map[i];
-  //     //   }
-  //     // }
-
-
-  //     userLocation = this.event.request.intent.slots.UScity.value;
-
-  //     this.response.speak(userLocation + " has been set");
-  //     this.emit(':responseReady');
-  //   },
-  'recordIntent': async function() {
+  'recordIntent': function() {
     let date = new Date();
     let startTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
     let dateStamp = `${date.getMonth()}-${date.getDate()}-${date.getFullYear()}`;
@@ -46,18 +27,17 @@ let handlers = {
         "date": dateStamp,
       }
     };
-    await ddb.put(params,(err,data)=>{
+    ddb.put(params,(err,data)=>{
       if (err){
         console.log(err)
       }else{
-        console.log(data)
+        this.emit(":ask", "Recording");
       }
-    }).promise();
-    this.emit(":ask", "Recording");
+    });
 
   },
-  'stopRecordingintent': async function(){
-    //failure here//
+
+  'stopRecordingintent': function(){
     let date = new Date();
     let endTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
     let params = {
@@ -72,16 +52,16 @@ let handlers = {
       },
       ReturnValues:"UPDATED_NEW"
     };
-    await ddb.update(params,(err,data)=>{
+    ddb.update(params,(err,data)=>{
       if (err){
         this.response.speak(err);
         this.emit(':responseReady');
       }else{
         this.emit(":ask", "Ending recording, say seizure type or skip to end session.");
       }
-    }).promise();
+    });
 
-  },'seizureTypeintent': async function(){
+  },'seizureTypeintent': function(){
     let date = new Date();
     let seizureCategory = this.event.request.intent.slots.seizure.value;
 
@@ -98,15 +78,16 @@ let handlers = {
       ReturnValues:"UPDATED_NEW"
     };
 
-    await ddb.update(params,(err,data)=>{
+    ddb.update(params,(err,data)=>{
       if (err){
         console.log(err)
+        this.response.speak("An error has occured.")
+        this.emit(':responseReady');
       }else{
         this.response.speak("Information has been recorded.")
         this.emit(':responseReady');
       }
-    }).promise();
-
+    });
     //end
   }
 }
